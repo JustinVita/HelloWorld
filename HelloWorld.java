13926fc28f3d0fe0d15fe2fe41824ae9919588b1
JustinVita <skeeisjus@gmail.com>
2016-07-15 23:35:32 -0400

    Code

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+   public static void main(String[]arg)
+   {
+      System.out.println ("Hello World");
+      System.out.println ("I can't wait to start this crap!"); 
+   }
+}
